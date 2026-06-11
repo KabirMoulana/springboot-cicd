5a59b80a1dbad6531ebe3ceab30535bec2225863
KabirMoulana <kabirmoulana41@gmail.com>
2026-06-11 07:43:06 +0000

    fix(test): add @Import(TestAsyncConfig.class) to ApplicationContextTest and ActuatorEndpointsTest

--- src/test/java/com/devops/app/ApplicationContextTest.java
+++ src/test/java/com/devops/app/ApplicationContextTest.java
@@ -1,11 +1,14 @@
 package com.devops.app;
 
+import com.devops.app.config.TestAsyncConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestAsyncConfig.class)
 class ApplicationContextTest {
 
     @Test
