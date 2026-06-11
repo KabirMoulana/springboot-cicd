699a5af61e9d6535554c34d997a8199afb939092
KabirMoulana <kabirmoulana41@gmail.com>
2026-06-11 02:13:00 +0000

    feat(config): add JpaConfig enabling JPA auditing, repository scan, transaction management

---
+++ src/main/java/com/devops/app/config/JpaConfig.java
@@ -0,0 +1,13 @@
+package com.devops.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.devops.app.repository")
+@EnableTransactionManagement
+public class JpaConfig {
+}
